84bd08473fe11459743acac3a87ebd5fc97b1b62
Vladimir Tsymbaliuk <89538231+RichardDitrix@users.noreply.github.com>
2023-10-28 17:53:32 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,69 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function gallery(galleryItems) {
+	return galleryItems.map(({preview, original, description}) => {
+		return `
+		<div class="gallery__item">
+	  <a class="gallery__link" href="large-image.jpg">
+		 <img
+			class="gallery__image"
+			src="${preview}"
+			data-source="${original}"
+			alt="${description}"
+		 />
+	  </a>
+	</div>
+		`;
+	}).join('');	
+}
+
+const divGallery = document.querySelector(".gallery");
+
+const galleryMarkUp = gallery(galleryItems);
+
+divGallery.insertAdjacentHTML("beforeend", galleryMarkUp);
+
+divGallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+	if (event.target.nodeName !== 'IMG') {
+		return;
+	 }
+	 event.preventDefault();
+	 modalShow(event.target.dataset.source);
+  }
+
+  let modal;
+function modalShow(src) {
+	modal = basicLightbox.create(
+    `
+    <div class="modal">
+        <img src="${src}" style="height:100vh; display:block"></img>
+    </div>
+`,
+    {
+      onShow: modal => {
+        addListener();
+      },
+      onClose: modal => {
+        removeListener();
+      },
+    },
+  );
+  modal.show();
+}
+
+function addListener() {
+  window.addEventListener('keydown', onEscClick);
+}
+
+function onEscClick(event) {
+  if (event.code === 'Escape') {
+	modal.close();
+  }
+}
+
+function removeListener() {
+  window.removeEventListener('keydown', onEscClick);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemMarkup = ({ preview, original, description }) => {
+	return `
+	<li>
+	  <a class="gallery__item" href="${original}">
+	  <img 
+	  style="display:block"
+	  class="gallery__image"
+	  src="${preview}"
+	  alt="${description}"
+	  />
+	</a>
+	</li>
+	`;
+ };
+ 
+ const finishGalleryMarkup = galleryItems.map(galleryItemMarkup).join('');
+ 
+ const galleryElements = document.querySelector('.gallery');
+ 
+ galleryElements.insertAdjacentHTML('beforeend', finishGalleryMarkup);
+ 
+ let finalGallery = new SimpleLightbox('.gallery a', {
+	captions: true,
+	captionsData: 'alt',
+	captionDelay: 250,
+ });
